cd2f8f5f5725ebeeb4abb783f18afbf03eec92e3
Hilary Valencia-Walsh <hilaryvalenciawalsh@gmail.com>
2020-10-12 20:21:45 -0700

    creating connection

--- index.js
+++ index.js
@@ -3,3 +3,28 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
 require('console.table');
+
+const promptMessages = {
+    viewAllEmployees: "View All Employees",
+    viewByDepartment: "View All Employees By Department",
+    viewByManager: "View All Employees By Manager",
+    updateRole: "Update Employee Role",
+    addEmployee: "Add An Employee",
+    removeEmployee: "Remove An Employee",
+    updateEmployeeManager: "Update Employee Manager",
+    viewAllRoles: "View All Roles",
+    exit: "Exit"
+};
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'employees'
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    prompt();
+});
